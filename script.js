6cc76b5f6dea4ab4db48562865b79d813da92eb5
suresh-skj <sureshjanwa20@gmail.com>
2025-03-06 11:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+ const btn = document.getElementById("btn");
+
+  btn.addEventListener("click", function (event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+
+    if (name === "" || age === "") {
+      alert("Please enter valid details");
+      return;
+    }
+
+    const ageNumber = parseInt(age, 10);
+
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (ageNumber > 18) {
+          resolve(`Welcome, ${name}. You can vote.`);
+        } else {
+          reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+      }, 4000); 
+    })
+      .then((message) => {
+        alert(message);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  });
